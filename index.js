7de0b6557cc4205417079370fa2949232ea76207
Filip Kaluza <kaluzafilip33@gmail.com>
2020-12-14 13:24:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path")
+
+// Init app
+const app = express();
+
+// Load view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Routing
+app.get("/", (req, res) => {
+    res.send("Hello World")
+});
+
+// Start server
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server started" );
+
+});
